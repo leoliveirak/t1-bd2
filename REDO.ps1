81c9678ca226ce22439d5aa1edfd6a271043a288
Gustavo Botezini <114600733+Gustavo-Botezini@users.noreply.github.com>
2025-05-27 14:42:48 -0300

    Update REDO.ps1

--- REDO.ps1
+++ REDO.ps1
@@ -7,3 +7,23 @@ $env:PGPORT = "5432"
 $env:PGDATABASE = "DATABASE"
 $env:PGUSER = "USUÁRIO"
 $env:PGPASSWORD = $senhaTexto
+
+# Busca completa e cronológica
+$comando = @"
+	SELECT l.log_id, l.acao, l.operation_id, l.nome, l.saldo 
+	FROM logs_operacao l 
+	ORDER BY l.log_id
+"@
+
+$log = psql -t -A -F "|" -c $comando
+<# 
+-t -> Tira o cabeçalho 
+-A -> Tira a formatação
+-F -> Serve para separar os campos
+-c -> Comando a ser executado
+#>
+
+# Variáveis de controle
+$executarBloco = $false
+$bloco = @()
+$blocosValidos = @()
