8ad213e517cc3fab8297e6d6d42c222de6908779
Gustavo Botezini <114600733+Gustavo-Botezini@users.noreply.github.com>
2025-05-27 14:42:05 -0300

    Update REDO.ps1

--- REDO.ps1
+++ REDO.ps1
@@ -1,1 +1,9 @@
-
+# Solicita a senha do usuário uma única vez
+$senha = Read-Host -AsSecureString "Digite a senha do usuario postgreSQL"
+$senhaTexto = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($senha))
+# Configurações :)
+$env:PGHOST = "localhost"
+$env:PGPORT = "5432"
+$env:PGDATABASE = "DATABASE"
+$env:PGUSER = "USUÁRIO"
+$env:PGPASSWORD = $senhaTexto
